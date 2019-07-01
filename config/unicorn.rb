c57bf55831c122c2d5df18fa3862040a92d4c32a
moemi ohtsu <s0623192@gmail.com>
2019-07-01 11:15:00 +0900

    Add: unicornローカル確認ok,本番用にunicorn.rbを書き換え

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,29 +1,53 @@
+# Rails のルートパスを設定（RAILS_ROOT/config/unicorn.rbに配置している場合）
 rails_root = File.expand_path('../../', __FILE__)
 
+# RAILS_ENV を設定(RAILS_ENVごとに挙動を変えたい場合に使用。)
+# rails_env = ENV['RAILS_ENV'] || 'development'
+
+# capistrano の unicorn restart のエラー解消
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{rails_root}/Gemfile"
+end
+
+# ワーカー数の定義
 worker_processes 2
+
+# Unicornの起動コマンドを実行するディレクトリを指定
 working_directory rails_root
 
-listen "#{rails_root}/tmp/sockets/unicorn.sock"
-pid "#{rails_root}/tmp/pids/unicorn.pid"
+# 接続タイムアウト時間
+timeout 30
 
+# Unicornのエラーログと通常ログの吐き出しファイルを指定
+stderr_path File.expand_path('../../log/unicorn_stderr.log', __FILE__)
+stdout_path File.expand_path('../../log/unicorn_stdout.log', __FILE__)
+
+# Nginxで使用する場合はこちらの設定
+listen File.expand_path('../../tmp/sockets/unicorn.sock', __FILE__)
+# ひとまずローカルで8080
+# listen 8080
+
+# プロセス停止などに必要なPIDファイルの保存先を指定
+pid File.expand_path('../../tmp/pids/unicorn.pid', __FILE__)
+
+# Unicorn再起動時にダウンタイムなしで再起動を設定
+preload_app true
+
+# USR2シグナルを受けると古いプロセスを止める
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
   defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+      ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
-
-stderr_path "#{rails_root}/log/unicorn_error.log"
-stdout_path "#{rails_root}/log/unicorn.log"
